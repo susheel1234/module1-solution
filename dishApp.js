4f819c52f99ae3a7ba9f312da44c589d188b1a46
susheel yadav <susheelyadavsusheel@gmail.com>
2019-03-15 22:57:28 +0530

    after including file

--- dishApp.js
+++ dishApp.js
@@ -1,7 +1,18 @@
 (function(){
   'use strict';
-  angular.module("dishChecker", []).controller("dishCheckerController",function($scope){
+  angular.module("dishChecker", []).controller("dishCheckerController",dishCheckerController);
+  dishCheckerController.$inject=["$scope"];
+
+  function dishCheckerController($scope){
     $scope.message="";
-    
-  })
+    $scope.checkIfTooMuch=function(){
+      var returnMessage=logicFuntion($scope.name);
+      $scope.message=returnMessage;
+    }
+  }
+
+  function logicFuntion(item){
+    var arrayItem=item.split(",");
+    return arrayItem[0];
+  }
 })();
